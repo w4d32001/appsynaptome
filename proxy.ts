a465f9c572d2cbbaf1e04ac4fb9b1cdf28624543
w4d32001 <freedom01022001@gmail.com>
2026-05-13 21:34:05 -0500

    Fix: Correccion de la implementacion de Clerk para autenticacion

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,22 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+	'/(auth)/sign-in(.*)',
+	'/(auth)/sign-up(.*)',
+	'/sign-in(.*)',
+	'/sign-up(.*)'
+])
+
+export default clerkMiddleware(async (auth, req) => {
+	if (!isPublicRoute(req)) {
+		await auth.protect()
+	}
+})
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
+	matcher: [
+		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+		'/(api|trpc)(.*)',
+		'/__clerk/(.*)',
+	],
+}
